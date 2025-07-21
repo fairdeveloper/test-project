efb8cf1ea9d7898faae6e2723c5b5dec4b43754b
Oğulcan Evren <ogulcanevren@Mac.home>
2025-07-21 03:40:36 +0200

    BUILD: Lansman icin son ve nihai

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const config = {
+  // Buraya gelecekte özel Next.js ayarları eklenebilir.
+  // Şimdilik boş kalması normaldir.
+};
+
+export default config;
